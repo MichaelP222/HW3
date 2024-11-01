dc960a83cd373c42cb5f603d7de33e06101b44d6
Yinuo Peng <yinuop2@uci.edu>
2024-11-01 23:14:49 +0000

    complete all functions of player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,35 @@
 // include the header file here
+#include "../include/player.h"
+#include <iostream>
+
 
 // add all your player functions in here.
+Player *newPlayer(std::string name, int num_goals){
+    Player *new_player = new Player;
+    new_player->name = name;
+    new_player->num_goals = num_goals;
+    return new_player;
+}
+
+Player *copyPlayer(const Player *p){
+    Player *copy_player = new Player;
+    copy_player->name = p->name;
+    copy_player->num_goals = p->num_goals;
+    return copy_player;
+}
+
+void printPlayer(const Player *p){
+    if (p == nullptr){
+        std::cout << "No player found!" << std::endl;
+    }else{
+        std::cout << "Player name: " << p->name << std::endl;
+        std::cout << "Total goals: " << p->num_goals << std::endl;
+    }
+    return;
+}
+
+void deletePlayer(Player *p){
+    delete p;
+    p = nullptr;
+    return;
+}
